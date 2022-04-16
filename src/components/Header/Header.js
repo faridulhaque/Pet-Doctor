6c70b8e113971e08623eb68b35f6e5c8dd545e34
faridulhaque <faridulhaquemurshed@gmail.com>
2022-04-17 00:58:42 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container-fluid">
+        {/* name and brand ----------------------------------------------------------------
+        -------------------------------------------------------*/}
+        <Link className="navbar-brand" to="/">
+          Pet Doctor
+        </Link>
+
+        {/* responsive navbar toggle button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* All navbar items created with vanilla bootstrap */}
+
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto me-4">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/blog">
+                Blog
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="about">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="signIn">
+                Sign In
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* navbar ended here */}
+        
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/components/NoFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center text-danger mt-5">Error 4o4</h1>
+            <p className="text-center text-danger mt-5">The page you are searching for is either deleted or was never created</p>
+        </div>
+    );
+};
+
+export default NotFound;
